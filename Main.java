95a1fef11ef00a42783f2cce38d4180987891729
aron.laika <aron.laika@gmail.com>
2026-01-14 18:50:16 +0100

    feat: actualizo el saludo desde local

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola equipo, desde mi rama.");
+        System.out.println("Hola universo, este es el cambio del junior");
     }
 }
