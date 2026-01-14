cea8b740566ebcf07ee8a13b60ea09a4567d05f6
Zeniwui <75743484+Zeniwui@users.noreply.github.com>
2026-01-14 18:47:59 +0100

    Update print statement in Main class

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola equipo, desde mi rama.");
+        System.out.println("Cambio del senior");
     }
 }
